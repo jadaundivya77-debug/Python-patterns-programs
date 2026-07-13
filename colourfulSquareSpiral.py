49ec8ac483ad40e56e273c759b3123cca9bd2609
jadaundivya77-debug <jadaundivya77@gmail.com>
2026-07-13 20:31:54 +0530

    colourfulSquareSpiral

---
+++ colourfulSquareSpiral.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+colors = ["red", "blue", "green", "purple", "orange"]
+
+t.speed(0)
+
+for i in range(150):
+    t.pencolor(colors[i % 5])
+    t.bgcolor("black")
+    t.forward(i * 2)
+    t.left(91)
+
+t.done()
