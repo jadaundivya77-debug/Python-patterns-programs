32b297602373663b484f0d5a785149fc71813e82
jadaundivya77-debug <jadaundivya77@gmail.com>
2026-07-17 12:32:46 +0530

    Add purple heart

---
+++ Heart.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+t.speed(0)
+t.color("purple")
+t.hideturtle()
+t.bgcolor("black")
+
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.left(120)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+
+t.done()
